2a47baca56664a15eeafbe66404eb0c1b148cb92
danhpham2000 <reepham2020@gmail.com>
2026-01-30 11:21:22 -0600

    Add endpoints for frontend

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,23 @@ import { Input } from "@/components/ui/input";
 import { Loader2 } from "lucide-react";
 import { useState } from "react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+
+type Result = {
+  content: string;
+  source: string;
+};
 
 export default function DocumentUpload() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoadingResult, setIsLoadingResult] = useState<boolean>(false);
   const [message, setMessage] = useState<{
     type: "error" | "success";
     text: string;
   } | null>(null);
+
+  const [query, setQuery] = useState<string>("");
+  const [result, setResult] = useState<Result>();
 
   const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -55,6 +65,29 @@ export default function DocumentUpload() {
       setIsLoading(false);
     }
   };
+
+  const handleAskQuery = async (e: any) => {
+    e.preventDefault();
+    const result = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/query`,
+      {
+        body: JSON.stringify({ query: query }),
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await result.json();
+
+    setQuery("");
+
+    setResult({
+      content: data.response,
+      source: data.sources,
+    });
+  };
   return (
     <div className="mt-5 p-10">
       <div>
@@ -90,9 +123,34 @@ export default function DocumentUpload() {
           )}
         </div>
 
-        <div>
-          <h1>Chat with your documents</h1>
-          
+        <div className="mt-30">
+          <h1 className="text-lg mb-5 font-medium text-primary">
+            Chat with your documents
+          </h1>
+          <Input
+            type="text"
+            name="query"
+            placeholder="Type your query"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+          />
+          <Button className="mt-5" onClick={handleAskQuery} type="submit">
+            Ask
+          </Button>
+          <div className="">
+            <h1 className="text-md mt-5 font-medium text-primary">Response</h1>
+            {result && (
+              <div className="">
+                <p>{result.content}</p>
+                <div>
+                  <h1 className="text-md font-medium text-primary mt-3">
+                    Source
+                  </h1>
+                  <p>{result.source}</p>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
